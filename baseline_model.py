346b3e7e9d7a088154265aa7ce0cf052e8c1f173
Elden Ren <eldenbr2015@gmail.com>
2022-10-16 03:11:35 -0700

    updated baseline_model and mle_model

--- baseline_model.py
+++ baseline_model.py
@@ -16,16 +16,24 @@ transformed_data = pca.fit_transform(encoded_data)
 bt = BallTree(transformed_data)
 
 def get_most_similar(pkgs):
-    transformed_package = pca.transform(pkgs)
+    transformed_package = pca.transform(np.reshape(pkgs, (1, -1)))
     return bt.query(transformed_package, k=100)
 
 def recommend_package(pkgs, recs):
-    adj = get_most_similar(pkgs)
-    best_pop = -1
-    ret = recs[0]
+    dist, adj = get_most_similar(pkgs)
+    adj = np.reshape(adj, -1)
+    print(adj)
+    print(encoded_data[adj].shape)
+    ret = []
     for rec in recs:
-        pop = np.sum(encoded_data[adj,rec])
-        if pop > best_pop:
-            best_pop = pop
-            ret = rec
+        pop = np.sum(encoded_data[adj][:,rec])
+        ret.append((-pop, rec))
+    ret = sorted(ret)
     return ret
+
+if __name__ == "__main__":
+    cur_pkgs = [1606, 1607, 1201, 562, 18, 19, 62]
+    recs = list(range(1608, 1750))
+    pkgs = np.zeros(num_packages)
+    pkgs[cur_pkgs] = 1
+    print(recommend_package(pkgs, recs))
--- mle_model.py
+++ mle_model.py
@@ -1,3 +1,4 @@
+from base64 import encode
 import numpy as np
 import pickle
 from sklearn.random_projection import johnson_lindenstrauss_min_dim, GaussianRandomProjection
@@ -9,6 +10,9 @@ print(num_packages)
 encoded_data = np.zeros((len(pkg_samples), num_packages))
 for i in range(len(pkg_samples)):
     encoded_data[i][list(pkg_samples[i])] = 1
+
+pkg_indices = [1,2,3]
+print(np.cov(encoded_data[:, pkg_indices].T))
 
 transformer = GaussianRandomProjection(n_components=120)
 transformed_data = transformer.fit_transform(encoded_data)
@@ -22,11 +26,26 @@ cov_inv = np.linalg.inv(cov)
 def log_likelihood(encoding):
     return (encoding - mean) @ (cov_inv @ (encoding - mean))
 
-'''
-plt.hist(np.minimum(np.array([log_likelihood(encoding) for encoding in transformed_data]), 300*np.ones(len(transformed_data))), bins=60)
-plt.show()
-'''
 
-plt.imshow(np.cov(encoded_data.T))
-plt.colorbar()
-plt.show()
+def recommend_package(pkgs, recs):
+    ret = []
+    for rec in recs:
+        if rec == 1608:
+            print('good')
+        if pkgs[rec] == 1:
+            return rec
+        pkgs_copy = pkgs.copy()
+        pkgs_copy[rec] += 1
+        transformed_pkgs = np.reshape(transformer.transform(np.reshape(pkgs_copy, (1, -1))), -1)
+        score = log_likelihood(transformed_pkgs)
+        ret.append((score, rec))
+    ret = sorted(ret)
+    return ret
+
+if __name__ == "__main__":
+    cur_pkgs = [1606, 1607, 1201, 562, 18, 19, 62]
+    recs = list(range(1608, 1750))
+    pkgs = np.zeros(num_packages)
+    pkgs[cur_pkgs] = 1
+    print(recommend_package(pkgs, recs))
+
