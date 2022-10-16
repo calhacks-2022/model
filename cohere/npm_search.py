4794fa08ed555301b034554f96f0f79a1ee77563
yuezy1234 <yuezy2015@gmail.com>
2022-10-16 01:45:43 -0700

    Finished layer_1

--- cohere/npm_search.py
+++ cohere/npm_search.py
@@ -1,18 +1,15 @@
 import requests
 
-API_KEY = ""
-with open('../apikey.txt') as f:
-    API_KEY = f.readline()
-
-def search_npm(queries):
+def search_npm(api_key, queries):
     ret = set()
     for query in queries:
-        r = requests.get(f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx=626e50bc7dd2043a2&q={query}&fields=items(link)")
+        r = requests.get(f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx=626e50bc7dd2043a2&q={query}&fields=items(link)")
         res = r.json()
-        results = [item['link'].split('/')[-1] for item in res['items']]
+        if 'items' not in res:
+            print("Error")
+            print(queries)
+            return []
+        results = [item['link'].split('package/')[-1].split('/v/')[0] for item in res['items']]
         for result in results:
             ret.add(result)
-    return ret
-
-if __name__ == "__main__":
-    print(list(search_npm(["react", "react-dropdown"])))
+    return list(ret)
